a97e6ca977393e8afbffa0689d7103817104bdcf
Porrith Suong <psuong95@gmail.com>
2016-10-17 16:03:50 -0400

    Adds templates.

--- question_2/app.py
+++ question_2/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Search query paths
 google_search_url = "https://www.google.com/#q="
@@ -7,11 +7,13 @@ duckduck_go_search_url = "http://www.bing.com/search?q="
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
-    # TODO: Make a search form
-    # TODO: Make a get request and scrape the web
-    pass
+    print request.method
+    print request.form
+    return render_template("base.html")
+
 
 if __name__ == '__main__':
     app.run()
