55261231287ca991fd35d81cd9a41b74a224bb23
Porrith Suong <psuong95@gmail.com>
2016-10-17 17:31:51 -0400

    Adds function templates for grabbing data.

--- question_2/app.py
+++ question_2/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from urllib2 import urlopen
+from BeautifulSoup import BeautifulSoup
 
 
 # Search query paths
@@ -20,6 +21,17 @@ def query():
     if request.method == 'POST':
         search_query = request.form['query']
 
+
+def get_google_data(url):
+    pass
+
+
+def get_bing_data(url):
+    pass
+
+
+def get_duck_data(url):
+    pass
 
 if __name__ == '__main__':
     app.run(debug=True)
